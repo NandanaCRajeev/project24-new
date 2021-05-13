11af1e9893f0775c32920fa144eb80bbb60d6ae8
NandanaCRajeev <83021680+NandanaCRajeev@users.noreply.github.com>
2021-05-13 19:11:05 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,9 @@
+class Iron {
+    constructor(x, y) {
+      var options = {
+        'density':0.8,
+        'friction': 3,
+        'restitution':30,
+      }
+    }
+}
---
+++ stone.js
@@ -0,0 +1,11 @@
+class Stone{
+	constructor(x,y,r){
+     var option = {
+		 'restitution':0.8,
+		 'friction':0.9,
+		 'density':12,
+   Matter.Body.setAngle(this.Body,angle)
+
+	 }
+    }
+}
